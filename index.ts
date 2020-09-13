93e3cf3302109acb67cf5bbb7e9af1e72e59fb2a
John Hershberg <johnher@gmail.com>
2020-09-13 14:42:01 -0700

    Added knex database

--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
 import express from "express";
 import cors from "cors";
 
+var database = require("knex")({
+  client: "pg",
+  connection: {
+    host: "ec2-52-72-65-76.compute-1.amazonaws.com",
+    user: "xyiihuvayytyhr",
+    password: "4cc27f1380333718ab92600d389eebb92ac061ac410cd5ac63bd88a7df989791",
+    database: "d2emsvsn090cem",
+    ssl: { rejectUnauthorized: false },
+  },
+});
+
 const app = express();
 const PORT = 3001;
 
@@ -8,6 +19,14 @@ app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Express + TypeScript Server!!");
+  console.log(req.body);
+  database("messages")
+    .insert({
+      chat_message: "test",
+      from_user: "testuser",
+      time: new Date(),
+    })
+    .then(console.log);
   console.log(`Received`);
 });
 
