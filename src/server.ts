7ded673a3910eca86f7a46faa2a2579e7a8965e2
John Hershberg <johnher@gmail.com>
2020-09-13 17:19:06 -0700

    Added login Post route

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
 import socketio from "socket.io";
+import bodyParser from "body-parser";
 import http = require("http");
 
-import {addUser} from "./users";
+import { addUser } from "./users";
 
 const database = require("knex")({
   client: "pg",
@@ -19,6 +20,7 @@ const database = require("knex")({
 /** Server definitions */
 const PORT = 3001;
 const app = express();
+const jsonParser = bodyParser.json();
 const server = http.createServer(app);
 const io = socketio(server);
 
@@ -27,11 +29,11 @@ app.use(cors());
 io.on("connection", (socket) => {
   console.log("We have a new connection");
 
-  socket.on("join", (name: string, callback: Function)=>{
+  socket.on("join", (name: string, callback: Function) => {
     const newUser = addUser(name);
 
     if (newUser.error) return callback(newUser.error);
-  })
+  });
 });
 
 app.get("/", (req, res) => {
@@ -45,6 +47,13 @@ app.get("/", (req, res) => {
   //   })
   //   .then(console.log);
   // console.log(`Received`);
+});
+
+app.post("/login", jsonParser, (req, res) => {
+  const newUser: { newUser: string } = req.body;
+  const result = addUser(newUser.newUser);
+
+  res.send(result);
 });
 
 server.listen(PORT, () => {
